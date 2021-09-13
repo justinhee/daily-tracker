a0443d5e4855528cb2df73772df0b41bb1871afa
Justin Hee <justinhee118@gmail.com>
2021-09-12 21:52:01 -0700

    added login

--- src/components/editDay/EditDay.jsx
+++ src/components/editDay/EditDay.jsx
@@ -3,10 +3,17 @@ import './editDay.css'
 export default function EditDay(props) {
     return (props.trigger) ? (
         <div className="editDay">
-            <div className="popup-inner">
-                <button className="close-button" onClick={props.setTrigger}>close</button>
-                <span className="date">{props.month+1} {props.day} {props.year}</span>
+            <div className="popupWrapper">
+                <div className="popup-inner">
+                    <button className="quit-button" onClick={props.setTrigger}>quit</button>
+                    <div className="date">{props.month + 1} {props.day} {props.year}</div>
+                    <form className="dayForm">
+                        <textarea placeholder="how was your day..." type="text" className="editText"></textarea>
+                        <button className="editSave">save</button>
+
+                    </form>
+                </div>
             </div>
         </div>
-    ) : "";
+    ) : <></>;
 }
